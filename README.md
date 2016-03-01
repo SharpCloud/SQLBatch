e4ad61f3c3b469171fb9dbcb1f6e501246f5eae3
Russell Johnson <rusty@sharpcloud.com>
2016-03-01 23:38:13 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SQLBatch
 Very simple way to update a SharpCloud Story from a database
+Simply setup the values in the config file and run your queries to update SharpCloud.
+Schedule the program to run at specific periods (e.g. every hour) to make sure your story is always up to date
---
+++ SCSQLBatch/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using SC.API.ComInterop;
+
+namespace SCSQLBatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userid = ConfigurationManager.AppSettings["userid"];
+            var password = ConfigurationManager.AppSettings["password"];
+            var url = ConfigurationManager.AppSettings["url"];
+            var storyid = ConfigurationManager.AppSettings["storyid"];
+            var connectionString = ConfigurationManager.AppSettings["connectionString"];
+            var queryString = ConfigurationManager.AppSettings["queryString"];
+
+            // basic checks
+            if (string.IsNullOrEmpty(userid) || userid == "USERID")
+            {
+                Console.WriteLine("Error: No username provided.");
+                return;
+            }
+            if (string.IsNullOrEmpty(password))
+            {
+                Console.WriteLine("Error: No password provided.");
+                return;
+            }
+            if (string.IsNullOrEmpty(url))
+            {
+                Console.WriteLine("Error: No URL provided.");
+                return;
+            }
+            if (string.IsNullOrEmpty(storyid) || userid == "00000000-0000-0000-0000-000000000000")
+            {
+                Console.WriteLine("Error: No storyID provided.");
+                return;
+            }
+            if (string.IsNullOrEmpty(connectionString) || connectionString == "CONNECTIONSTING")
+            {
+                Console.WriteLine("Error: No connection string provided.");
+                return;
+            }
+            if (string.IsNullOrEmpty(queryString) || userid == "QUERYSTRING")
+            {
+                Console.WriteLine("Error: No database query provided.");
+                return;
+            }
+
+            // do the work
+
+            try
+            {
+                // create our connection
+                var sc = new SharpCloudApi(userid, password, url);
+                var story = sc.LoadStory(storyid);
+
+                using (DbConnection connection = GetDb(connectionString))
+                {
+                    connection.Open();
+                    using (DbCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = queryString;
+                        command.CommandType = CommandType.Text;
+
+                        using (DbDataReader reader = command.ExecuteReader())
+                        {
+                            var tempArray = new List<List<string>>();
+                            while (reader.Read())
+                            {
+                                var objs = new object[reader.FieldCount];
+                                reader.GetValues(objs);
+                                var data = objs.Select(o => o.ToString()).ToList();
+                                tempArray.Add(data);
+                            }
+
+                            // create our string arrar
+                            var arrayValues = new string[tempArray.Count + 1, reader.FieldCount];
+                            // add the headers
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                arrayValues[0, i] = reader.GetName(i);
+                            }
+                            // add the data values
+                            int row = 1;
+                            foreach (var list in tempArray)
+                            {
+                                int col = 0;
+                                foreach (string s in list)
+                                {
+                                    arrayValues[row, col++] = s;
+                                }
+                                row++;
+                            }
+
+                            // pass the array to SharpCloud
+                            string errorMessage;
+                            if (story.UpdateStoryWithArray(arrayValues, false, out errorMessage))
+                            {
+                                story.Save();
+                                Console.WriteLine(string.Format("{0} rows updated.", row));
+                            }
+                            else
+                            {
+                                Console.WriteLine(errorMessage);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+        private static DbConnection GetDb(string connectionString)
+        {
+            var dbType = ConfigurationManager.AppSettings["dbType"];
+            switch (dbType)
+            {
+                default:
+                case "SQL":
+                    return new SqlConnection(connectionString);
+                case "ODBC":
+                    return new OdbcConnection(connectionString);
+                case "OLEDB":
+                    return new OleDbConnection(connectionString);
+            }
+        }
+    }
+}
